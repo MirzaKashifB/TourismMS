0b8b90813f87254d7f97386f73ad9ca6345420c1
kashif <mirzakashifb52@gmail.com>
2023-08-03 17:23:44 +0530

    commit5

--- test/specs/practice/goIbibo.js
+++ test/specs/practice/goIbibo.js
@@ -5,11 +5,15 @@ describe('calender test',()=>{
         await browser.$('//span[@class="logSprite icClose"]').click()
         await browser.$("//span[text()='From']").click();
         await browser.$('//span[@class="sc-12foipm-39 hMvqOa"]/../input').setValue('Bengaluru');
-        console.log("1111111111-----------1111111111");
-       await browser.$("//span[text()='Bengaluru, India']").click()
+        await browser.$("//span[text()='Bengaluru, India']").click()
         await browser.pause(2000);
-        (await browser.$('//span[@class="sc-12foipm-39 hMvqOa"]/../input')).setValue("delhi");
+        await browser.$('//span[@class="sc-12foipm-39 hMvqOa"]/../input').setValue("delhi");
         await browser.$('//span[text()="New Delhi, India"]').click();
+        await browser.$("//div[text()='September 2023']/../..//p[text()='5']").click()
+        await browser.$("//span[text()='Done']").click();
+        await browser.$("//a[text()='Done']").click();
+        await browser.$("//span[text()='Return']").click();
+        await browser.$("//div[text()='October 2023']/../..//p[text()='11']").click();
 
     })
 })
